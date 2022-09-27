451cc140ac520c1581c7dd0df4432d425450276f
unknown <dankinyi99@gmail.com>
2022-09-28 00:18:20 +0300

    first endpoint

---
+++ nestjs-course/rest-api/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { CoursesModule } from "./courses/courses.module";
+
+
+@Module({
+    imports:[
+        CoursesModule
+    ]
+})
+export class AppModule{
+
+}
---
+++ nestjs-course/rest-api/src/courses/controllers/courses.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get } from "@nestjs/common";
+import {Course} from '../../../../shared/course'
+import { findAllCourses } from '../../../db-data';
+
+@Controller()
+export class CoursesController{
+    @Get('/api/courses')
+    async findAllCourses(): Promise<Course[]>{
+
+        return findAllCourses();
+    }
+    
+    // @Get('/api/hello-world')
+    // async helloWorld(): Promise<string>{
+
+    //     return "hello world!"
+    // }
+}
